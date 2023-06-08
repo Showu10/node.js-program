a0584c09ec1d026b9bcba1c1365aebd946ebad02
Showaiz <e11894showaiz@dpsrkp.net>
2023-06-08 12:21:16 +0530

    Final change

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const fs = require('fs');
 
 let str = fs.readFileSync('./example.txt').toString();
 str = str.toLowerCase();
-while(str.includes(",", "  ")) {
-    str = str.replace(",", " ");
-    str = str.replace("  ", " ");
-}
 let word = String(process.argv.slice(2));
 word = word.toLowerCase();
-while(word.includes(",", "  ")) {
+while(word.includes(",")) {
     word = word.replace(",", " ");
-    word = word.replace("  ", " ");
+}
+while(word.includes("  ")) {
+    word = word.replace("  ", ", ");
 }
 if (str.includes(word)) {
     console.log("True");
